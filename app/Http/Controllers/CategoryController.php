ac6d00de70de3d3d3710165b770659c16e291fa5
SerhiiHerasymiuk <@DESKTOP-8DFR7S5>
2023-06-13 23:46:27 +0300

    Added Category CRUD

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function index() {
+        $list = Category::all();
+        return response()->json($list, 200,
+            ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'], JSON_UNESCAPED_UNICODE);
+    }
+
+    public function create(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|unique:categories|max:255',
+        ]);
+
+        $category = Category::create($validatedData);
+
+        return response()->json($category, 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|unique:categories|max:255',
+        ]);
+
+        $category = Category::findOrFail($id);
+        $category->update($validatedData);
+
+        return response()->json($category, 200);
+    }
+
+    public function delete($id)
+    {
+        $category = Category::findOrFail($id);
+        $category->delete();
+
+        return response()->json(null, 204);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get("/category", [CategoryController::class, "index"]);
+Route::post("/category", [CategoryController::class, "create"]);
+Route::put("/category/{id}", [CategoryController::class, "update"]);
+Route::delete("/category/{id}", [CategoryController::class, "delete"]);
